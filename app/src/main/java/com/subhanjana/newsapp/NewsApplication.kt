3a860b23d3fa98e5eed1127d15a0e55c3d1f11ee
Subhanjana <subh07maj@gmail.com>
2023-12-26 18:53:17 +0530

    Implement News Application Class

---
+++ app/src/main/java/com/subhanjana/newsapp/NewsApplication.kt
@@ -0,0 +1,24 @@
+package com.subhanjana.newsapp
+
+import android.app.Application
+import com.subhanjana.newsapp.di.components.ApplicationComponent
+import com.subhanjana.newsapp.di.modules.ApplicationModule
+
+class NewsApplication : Application() {
+
+    lateinit var applicationComponent: ApplicationComponent
+
+    override fun onCreate() {
+        super.onCreate()
+        injectDependencies()
+    }
+
+    private fun injectDependencies() {
+        applicationComponent = DaggerApplicationComponent
+            .builder()
+            .applicationModule(ApplicationModule(this))
+            .build()
+        applicationComponent.inject(this)
+    }
+
+}
